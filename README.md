4aa594662ac6bce53453c18ef627448ed916630d
dgellow <samuel.elborai@gmail.com>
2015-08-05 11:16:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+![tesuto logo](http://i.imgur.com/orh29FB.png)
+
+# tesuto
+
+A really tiny test reporting library, only 829 bytes.
+The only dependency is [marak/colors.js](https://github.com/marak/colors.js/).
+
+## Usage
+
+```js
+var assert = require('assert'),
+    t = require('tesuto');
+
+function add (x, y) {
+  return x + y;
+}
+
+t.report("add function", function () {
+  assert.equal(add(1, 2), 3);
+  assert.equal(add(1, 2), 4);
+});
+
+t.result();
+```
+
+## Output example
+
+![tesuto example](http://i.imgur.com/tmYZJZn.png)
---
+++ index.js
@@ -0,0 +1,30 @@
+/*global require, module, process*/
+var c = require('colors/safe');
+
+var counterTotal = 0,
+    counterSuccess = 0,
+    counterFailure = 0;
+
+module.exports.report = function report(testName, testFn) {
+  try {
+    counterTotal += 1;
+    process.stdout.write(c.yellow(testName + ': '));
+    testFn();
+    console.info(c.green('OK'));
+    counterSuccess += 1;
+  } catch(e) {
+    var match = e.stack.match(/at.+test.js:(\d+):(\d+)/),
+        line = match[1],
+        char = match[2];
+    counterFailure += 1;
+    console.error(c.red('FAILED') +' ' + c.blue(line + ':' + char));
+    console.error(c.red(e.message));
+    console.error(c.grey(e.stack));
+  }
+};
+
+module.exports.result = function result() {
+  console.log('Total: ' + counterTotal);
+  console.log('Passed: ' + counterSuccess);
+  console.log('Failed: ' + counterFailure);
+};
