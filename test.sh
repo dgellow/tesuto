b74a4b7e6eb4d37c0e71a70b89668d6260d81844
dgellow <samuel.elborai@gmail.com>
2015-08-05 19:39:43 +0200

    debugging travis

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 ACTUAL_STDOUT=$(node test.js)
-EXPECTED_STDOUT=$(echo -e "\e[33madd success: \e[39m\e[32mOK\e[39m
+EXPECTED_STDOUT=$(/bin/echo -e "\e[33madd success: \e[39m\e[32mOK\e[39m
 \e[33madd failure: \e[39m\e[31mFAILED\e[39m \e[34m16:10\e[39m
 \e[31m3 == 4\e[39m
 Total: 2
 Passed: 1
 Failed: 1")
+
+echo "___________"
+echo "echo --version"
+echo $(/bin/echo --version)
+echo "___________"
 
 echo "___________"
 echo "ACTUAL"
