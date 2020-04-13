5a4226285f1c06e11665c116e4a5e9a0eed88a33
Samuel El-Borai <samuel.elborai@gmail.com>
2020-04-13 21:02:26 +0200

    Remove dist directory from source control

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function report(testName: string, testFn: () => void): void;
-export declare function testing(groupName: string, groupFn: () => void): void;
-export declare function result(): number;
-declare const _default: {
-    report: typeof report;
-    testing: typeof testing;
-    result: typeof result;
-};
-export default _default;
--- dist/test/module1.js
+++
@@ -1,7 +0,0 @@
-import { equal } from 'assert';
-function recurse() {
-    recurse();
-}
-export default function testModule() {
-    equal(recurse(), "will fail");
-}
